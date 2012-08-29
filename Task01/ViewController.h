8e49371696462a069638a1e8d1b9c3c46f0673c9
Evgeniy Shuliak <evgeniy.shuliak@gmail.com>
2012-08-29 21:26:08 +0300

    project

---
+++ Task01/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Task01
+//
+//  Created by Evgeniy Shuliak on 29.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
